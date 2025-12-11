6e9568741f9e9df4371426e8ed87bc303175f6fc
Embari Sahith <embari.sahith@hcltech.com>
2025-12-11 11:56:58 +0530

    updated client

--- EmpJavaProject/src/com/guvi/demos/Client.java
+++ EmpJavaProject/src/com/guvi/demos/Client.java
@@ -5,6 +5,8 @@ public class Client {
 	public static void main(String[] args) {
 		System.out.println("line1");
 		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
 
 	}
 
